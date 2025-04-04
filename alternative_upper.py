0f332e19797e35b4e965d4f8128914485c54207a
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-04-04 16:59:14 +0800

    Asked user dto input their statement and convert the letters to uppercase by ccalling the function.

--- alternative_upper.py
+++ alternative_upper.py
@@ -11,7 +11,8 @@ def custom_upper(input_string):
             result += char
     return result
 #Ask user to input a statement
-
+user_statement = input("Enter a statement: ")
 #Convert to uppercase
-
+uppercase_statement = custom_upper(user_statement)
 #Display with result
+print("Uppercase statement:", uppercase_statement)
