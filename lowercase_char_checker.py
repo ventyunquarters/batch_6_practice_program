936d494d58d8fc7fdd8b48bdd995f072325f0a0a
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-04-04 17:13:40 +0800

    Asked user to input their statement

--- lowercase_char_checker.py
+++ lowercase_char_checker.py
@@ -6,3 +6,12 @@ def custom_islower(input_string):
         elif char.isalpha() and not ('a' <= char <= 'z'):
             return False #If its an alphabet but not lowercase
     return True
+
+#Ask user to input a statement
+user_statement = input("Enter a statement: ")
+
+# Check if the statement is all lowercase
+is_lowercase = custom_islower(user_statement)
+
+# Display the result
+print("Is the statement all lowercase?", is_lowercase)
