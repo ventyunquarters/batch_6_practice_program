811ee178df4793d83cad926a676f4a5e8157fe7c
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-04-04 16:48:24 +0800

    Initial commit of pseudocode

---
+++ alternative_upper.py
@@ -0,0 +1,11 @@
+#Define custom upper
+def custom_upper(input_string):
+
+#Use arg input string to convert and return uppercase version
+
+
+#Ask user to input a statement
+
+#Convert to uppercase
+
+#Display with result
