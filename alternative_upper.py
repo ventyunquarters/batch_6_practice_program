ff2e6b8cc30438019664840981225513f5f3c14f
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-04-04 16:57:05 +0800

    Added logic for the condition of converting smaller to uppercase

--- alternative_upper.py
+++ alternative_upper.py
@@ -2,8 +2,14 @@
 def custom_upper(input_string):
 
 #Use arg input string to convert and return uppercase version
-
-
+    result = ""
+    for char in input_string:
+        if 'a' <= char < 'z':
+            #Convert lowerase to upper using ASCII values
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
 #Ask user to input a statement
 
 #Convert to uppercase
